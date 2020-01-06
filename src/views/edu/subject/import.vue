e3b9ed4474c288360886cb1a7cc55e4a84db5d3b
lgg_gl <lgg_gl@qq.com>
2020-01-06 18:28:24 +0800

    init

---
+++ src/views/edu/subject/import.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'import',
+    data() {
+      return {
+        BASE_API: process.env.BASE_API, // 接口API地址
+        OSS_PATH: process.env.OSS_PATH, // 阿里云OSS地址
+        fileUploadBtnText: '上传到服务器', // 按钮文字
+        importBtnDisabled: false, // 按钮是否禁用,
+        loading: false
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
